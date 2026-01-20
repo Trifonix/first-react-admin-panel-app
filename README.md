fb72a8d2c0ce931fbcd16b82f04b721ed3f4dda1
Trifonix <trifonix@rocketmail.com>
2026-01-20 23:13:48 +0300

    add two routes

--- README.md
+++ README.md
@@ -12,3 +12,5 @@
 [20.01.2026] - Проект создан через **npm create vite@latest client-app -- --template react-ts**
 [20.01.2026] - Удалены лишние файлы
 [20.01.2026] - Созданы директории components, pages, services и заглушки страниц
+[20.01.2026] - Установлен роутинг через **npm install react-router-dom** в корне проекта
+[20.01.2026] - Добавлены два роута - Login и Dashboard
--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,24 +1,19 @@
-import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
+// import UserDetails from "./pages/UserDetails";
+// import Users from "./pages/Users";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
